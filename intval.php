0628e8c0f11ef6e2cee5fb8a1ab6532e4de8372a
Ogu <august.gros@gmail.com>
2022-04-19 19:10:21 +0200

    add intval non-fonctionnelle

---
+++ intval.php
@@ -0,0 +1,26 @@
+<?php
+include "./count.php";
+include "./strlen.php";
+
+function fn_intval($var){
+    $int = "/^[0-9\s]*$/";
+    $virgule = "/^[\-]{0,1}[0-9]+[\.][0-9]+|[\-]{0,1}[0-9]+$/";
+    if (preg_match($virgule,$var)) {
+        $res = "";
+        for ($i=0; $i < fn_strlen($var); $i++) { 
+            if ($var[$i] == ".") {
+                break;
+            }else{
+                $res .= $var[$i];
+            }
+        }
+        return $res;
+    }else if (preg_match($int,$var)) {
+        return $var;
+    }else{
+        return fn_count($var);
+    }
+}
+echo fn_intval(0.56);
+
+?>
